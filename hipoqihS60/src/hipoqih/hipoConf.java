ee696a21a21693957c8918bf6e7085112a4b77a2
xcanvic <xcanvic@d01ceb20-942c-0410-b683-cd95dc0ac709>
2007-05-04 12:18:54 +0000

    Se añade la llamada a la página web para recibir el código de alta.

--- hipoqihS60/src/hipoqih/hipoConf.java
+++ hipoqihS60/src/hipoqih/hipoConf.java
@@ -8,6 +8,7 @@ public class hipoConf extends Form implements CommandListener
 {
      javax.microedition.lcdui.TextField txtLogin = new TextField("TextField", "", 50, TextField.ANY);
      javax.microedition.lcdui.TextField txtClave = new TextField("TextField", "", 50, TextField.ANY);
+     static public final javax.microedition.lcdui.Command cmdSalir = new Command("Salir", Command.EXIT, 0);
 
      public hipoConf(String p1, Item[] p2)
      {
@@ -41,6 +42,8 @@ public class hipoConf extends Form implements CommandListener
           txtLogin.setLabel("Login");
           append(txtClave);
           txtClave.setLabel("Clave");
+          this.addCommand(cmdSalir);
+          CommandHandler.getInstance().registerCommand(cmdSalir, "hipoqih.hipoForm");
      }
 
      public void commandAction(Command command, Displayable displayable)
