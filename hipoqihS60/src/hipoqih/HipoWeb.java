4c1c32e786e4c565577089d347d44da2b92d515d
xcanvic <xcanvic@d01ceb20-942c-0410-b683-cd95dc0ac709>
2007-05-14 15:00:38 +0000

    Añadimos código para que conecte con la web con el mensaje de alta (sólo es una prueba, se limita a imprimir en el móvil el mensaje devuelto).

--- hipoqihS60/src/hipoqih/HipoWeb.java
+++ hipoqihS60/src/hipoqih/HipoWeb.java
@@ -1,11 +1,56 @@
 package hipoqih;
 
+import java.io.*;
 import java.io.IOException;
+import javax.microedition.io.*;
 
 public class HipoWeb
 {
-	public static String conectar() throws IOException
+	public static String conectar(String url) throws IOException
 	{
-		return "";
+		HttpConnection c = null;
+		InputStream is = null;
+		StringBuffer str = new StringBuffer(); // StringBuffer que almacenará la cadena de repuesta
+		
+		try
+		{
+			// Obtenemos la conexión con la dirección url indicada
+			c = (HttpConnection)Connector.open(url);
+			
+			// Obtener el código de respuesta abrirá la conexión,
+			// enviará la petición, y leerá las cabeceras de la respuesta HTTP.
+			int rc = c.getResponseCode();
+			if (rc != HttpConnection.HTTP_OK )
+			{
+				throw new IOException("Error HTTP:" + rc);
+			}
+
+			// Obtenemos el "stream" de datos
+			is = c.openInputStream();
+			
+		
+			// Leemos carácter a carácter hasta el final (-1)
+			int actual = -1;
+			while ((actual = is.read()) != -1)
+			{
+				str.append((char)actual);
+			}
+			
+			// Si no se devuelven datos, devolvemos un error
+			if (str.length() == 0)
+				throw new IOException("Unexpected error: empty response.");
+		}
+		catch (ClassCastException e)
+		{
+			throw new IllegalArgumentException("Not an HTTP URL");
+		}
+		finally
+		{
+			if ( is != null )
+				is.close();
+			if ( c != null )
+				c.close();
+		}
+		return str.toString();	
 	}
 }
