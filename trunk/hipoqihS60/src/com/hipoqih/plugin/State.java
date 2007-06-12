c6738f7f5503306189881a0813683e06564e2c3f
xcanvic <xcanvic@d01ceb20-942c-0410-b683-cd95dc0ac709>
2007-06-12 10:28:16 +0000

    We move rms management from the MIDlet to State class.

--- trunk/hipoqihS60/src/com/hipoqih/plugin/State.java
+++ trunk/hipoqihS60/src/com/hipoqih/plugin/State.java
@@ -2,12 +2,14 @@ package com.hipoqih.plugin;
  
 import java.util.Hashtable;
 
+import javax.microedition.rms.RecordEnumeration;
 import javax.microedition.rms.RecordStore;
+import javax.microedition.rms.RecordStoreException;
  
-//import com.hipoqih.plugin.gps.GPS;
-
 public class State 
 {
+	private final static State INSTANCE = new State();
+	
 	public static String user = "user";
 	public static String password = "pass";
 	public static boolean connected = false;
@@ -27,4 +29,149 @@ public class State
 	public static Hashtable recordMaps = new Hashtable();
 	public static int zoom = 10000;
 	public static String secureId = "";
+
+	private State() {}
+	
+	public static State getInstance()
+	{
+		return INSTANCE;
+	}
+	
+	// Load stored state
+	public void loadConfiguration() throws Exception
+	{
+		State.recordStore = RecordStore.openRecordStore("hipoqih", true);
+		
+		if (State.recordStore.getNumRecords() == 0)
+		{
+			createConfiguration(State.recordStore);
+		}
+		// Enumerate records without filter nor order
+		RecordEnumeration re = State.recordStore.enumerateRecords(null, null, false);
+		while(re.hasNextElement())
+		{
+			int id = re.nextRecordId();
+			byte[] record = State.recordStore.getRecord(id);
+			int typeId = record[0];
+			processRecord(record, typeId);
+			State.recordMaps.put(new Integer(typeId), new Integer(id));
+		}
+	}
+	
+	private void processRecord(byte[] record, int typeId)
+	{
+		// Each record type has a different id
+		switch(typeId)
+		{
+		case RecordTypes.ALERTHEIGHT:
+			State.alertHeight = bytesToInteger(record);
+			break;
+		case RecordTypes.ALERTLEFT:
+			State.alertLeft = bytesToInteger(record);
+			break;
+		case RecordTypes.ALERTTOP:
+			State.alertTop = bytesToInteger(record);
+			break;
+		case RecordTypes.ALERTWIDTH:
+			State.alertWidth = bytesToInteger(record);
+			break;
+		case RecordTypes.AUTOALERT:
+			State.autoAlert = bytesToBoolean(record);
+			break;
+		case RecordTypes.LATITUDE:
+			State.latitude = new String(record, 1, record.length - 1);
+			break;
+		case RecordTypes.LONGITUDE:
+			State.longitude = new String(record, 1, record.length - 1);
+			break;
+		case RecordTypes.MAPALERT:
+			State.mapAlert = bytesToBoolean(record);
+			break;
+		case RecordTypes.MINUTES:
+			State.minutes = new String(record, 1, record.length - 1);
+			break;
+		case RecordTypes.PASSWORD:
+			State.password = new String(record, 1, record.length - 1);
+			break;
+		case RecordTypes.POSITIONSOURCE:
+			State.positionSource = new String(record, 1, record.length - 1);
+			break;
+		case RecordTypes.SECONDS:
+			State.seconds = new String(record, 1, record.length - 1);
+			break;
+		case RecordTypes.URLMAPALERT:
+			State.urlMapAlert = bytesToBoolean(record);
+			break;
+		case RecordTypes.USER:
+			State.user = new String(record, 1, record.length - 1);
+			break;
+		}
+	}
+	
+	private boolean bytesToBoolean(byte[] record)
+	{
+		String s = new String(record, 1, record.length - 1);
+		return s.equals("1");
+	}
+	
+	private int bytesToInteger(byte[] record)
+	{
+		int result = 0;
+		String s = new String(record, 1, record.length - 1);
+		try
+		{
+			result = Integer.parseInt(s);
+		}
+		catch(NumberFormatException nfe)
+		{
+			System.out.println(nfe.getMessage());
+			nfe.printStackTrace();
+		}
+		return result;
+	}
+	
+	private void createConfiguration(RecordStore recordStore)
+	{
+		createRecord(recordStore, Integer.toString(State.alertHeight), RecordTypes.ALERTHEIGHT);
+		createRecord(recordStore, Integer.toString(State.alertLeft), RecordTypes.ALERTLEFT);
+		createRecord(recordStore, Integer.toString(State.alertTop), RecordTypes.ALERTTOP);
+		createRecord(recordStore, Integer.toString(State.alertWidth), RecordTypes.ALERTWIDTH);
+		String autoAlert = State.autoAlert ? "1" : "0";
+		createRecord(recordStore, autoAlert, RecordTypes.AUTOALERT);
+		createRecord(recordStore, State.latitude, RecordTypes.LATITUDE);
+		createRecord(recordStore, State.longitude, RecordTypes.LONGITUDE);
+		String mapAlert = State.mapAlert ? "1" : "0";
+		createRecord(recordStore, mapAlert, RecordTypes.MAPALERT);
+		createRecord(recordStore, State.minutes, RecordTypes.MINUTES);
+		createRecord(recordStore, State.password, RecordTypes.PASSWORD);
+		createRecord(recordStore, State.positionSource, RecordTypes.POSITIONSOURCE);
+		createRecord(recordStore, State.seconds, RecordTypes.SECONDS);
+		String urlMapAlert = State.urlMapAlert ? "1" : "0";
+		createRecord(recordStore, urlMapAlert, RecordTypes.URLMAPALERT);
+		createRecord(recordStore, State.user, RecordTypes.USER);
+	}
+	
+	private void createRecord(RecordStore recordStore, String data, int recordType)
+	{
+		int recordLength = 1;
+		byte[] dataBytes = data.getBytes();
+		
+		if (data.length() > 0)
+			recordLength = dataBytes.length + 1;
+		
+		byte[] record = new byte[recordLength]; 
+		record[0] = (byte)recordType;
+		for(int i = 1; i < recordLength; i++)
+			record[i] = dataBytes[i-1];
+		try
+		{
+			recordStore.addRecord(record, 0, recordLength);
+		}
+		catch(RecordStoreException rse)
+		{
+			System.out.println(rse.getMessage());
+			rse.printStackTrace();
+		}
+	}
+
 }
