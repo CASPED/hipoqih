fe6ad7552f4df27d23cd2751dc9699b83cde4a28
xcanvic <xcanvic@d01ceb20-942c-0410-b683-cd95dc0ac709>
2008-02-05 10:05:23 +0000

    Implemented options screen

--- trunk/blackberry/src/com/ipoki/plugin/blackberry/IpokiPlugin.java
+++ trunk/blackberry/src/com/ipoki/plugin/blackberry/IpokiPlugin.java
@@ -13,21 +13,132 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, If not, see <http://www.gnu.org/licenses/>.*/
 
+// hash of com.ipoki.plugin.blackberry.user
+// 0x57faa02a62ef6e45L
+// hash of com.ipoki.plugin.blackberry.pass
+// 0xe91da859a7ee3581L
+// hash of com.ipoki.plugin.blackberry.freq
+// 0x90b4a6286eb92c20L
 package com.ipoki.plugin.blackberry;
 
+import javax.microedition.rms.*;
 import net.rim.device.api.ui.*;
 import net.rim.device.api.ui.container.*;
 import net.rim.device.api.ui.component.*;
 import net.rim.device.api.i18n.*;
+import net.rim.device.api.system.*;
+import net.rim.device.api.ui.text.*;
 import com.ipoki.plugin.blackberry.resource.*;
 
-public class IpokiPlugin  extends UiApplication
+public class IpokiPlugin  extends UiApplication implements IpokiPluginResource
 {
     private IpokiMainScreen _mainScreen;
     
     PopupScreen _gaugeScreen;
     GaugeField _gauge;
     LabelField _label;
+    
+    static ResourceBundle _resources = ResourceBundle.getBundle(BUNDLE_ID, BUNDLE_NAME);
+    static PersistentObject _userStore;
+    static PersistentObject _passStore;
+    static PersistentObject _freqStore;
+    static String _user;
+    static String _pass;
+    static int _freq;
+    
+    static 
+    {
+        _userStore = PersistentStore.getPersistentObject(0x57faa02a62ef6e45L);
+        _passStore = PersistentStore.getPersistentObject(0xe91da859a7ee3581L);
+        _freqStore = PersistentStore.getPersistentObject(0x90b4a6286eb92c20L);
+        
+        if(_userStore.getContents()!= null && _passStore.getContents()!= null && _freqStore.getContents()!= null)
+        {
+            _user = (String)_userStore.getContents();
+            _pass = (String)_passStore.getContents();
+            _freq = Integer.parseInt((String)_freqStore.getContents());
+        }
+        else
+        {
+            _user = "";
+            _pass = "";
+            _freq = 10;
+        }
+    }
+    
+    static void SaveOptions(String user, String pass, String freq)
+    {
+        synchronized(_userStore)
+        {
+            _userStore.setContents(user);
+            _userStore.commit();
+            _user = user;
+
+        }
+        synchronized(_passStore)
+        {
+            _passStore.setContents(pass);
+            _passStore.commit();
+            _pass = pass;
+        }
+        synchronized(_freqStore)
+        {
+            _freqStore.setContents(freq);
+            _freqStore.commit();
+            _freq = Integer.parseInt(freq);
+        }
+    }
+    
+    private class SetupScreen extends MainScreen
+    {
+        private LabelField _userLabel;
+        private EditField _userEdit;
+        private LabelField _passLabel;
+        private PasswordEditField _passEdit;
+        private LabelField _freqLabel;
+        private EditField _freqEdit;
+        
+        public SetupScreen() 
+        {    
+            _userLabel = new LabelField(IpokiPlugin._resources.getString(LBL_USER), DrawStyle.ELLIPSIS);
+            add(_userLabel);
+            _userEdit = new EditField("", IpokiPlugin._user, 20, Field.EDITABLE);
+            add(_userEdit);
+            _passLabel = new LabelField(IpokiPlugin._resources.getString(LBL_PASSWORD), DrawStyle.ELLIPSIS);
+            add(_passLabel);
+            _passEdit = new PasswordEditField("", IpokiPlugin._pass, 20, Field.EDITABLE);
+            add(_passEdit);
+            _freqLabel = new LabelField(IpokiPlugin._resources.getString(LBL_FREQ), DrawStyle.ELLIPSIS);
+            add(_freqLabel);
+            _freqEdit = new EditField("", String.valueOf(IpokiPlugin._freq), 20, Field.EDITABLE | EditField.FILTER_INTEGER);
+            add(_freqEdit);
+        }
+        
+        private MenuItem _cancel = new MenuItem(IpokiPlugin._resources, MNU_CANCEL,  300000, 10) {
+            public void run()
+            {
+                 SetupScreen.this.close();
+            }
+        };  
+        
+        private MenuItem _save = new MenuItem(IpokiPlugin._resources, MNU_SAVE, 200000, 10) {
+            public void run()
+            {   
+                IpokiPlugin.SaveOptions(_userEdit.getText(), _passEdit.getText(), _freqEdit.getText());
+                IpokiPlugin.this.popScreen(SetupScreen.this);
+            }
+        };
+        
+         
+        protected void makeMenu( Menu menu, int instance )
+        {
+            menu.add(_save);
+            menu.add(_cancel);
+            
+            super.makeMenu(menu, instance);
+        }
+    }
+
     
     StatusThread _statusThread = new StatusThread();
     ConnectionThread _connectionThread = new ConnectionThread();
@@ -49,6 +160,12 @@ public class IpokiPlugin  extends UiApplication
         _connectionThread.start();
         
         pushScreen(_mainScreen);
+
+        if (_user == "")
+        {
+            SetupScreen setupScreen = new SetupScreen();
+            pushScreen(setupScreen);
+        }
     }
     
     public void connect()
